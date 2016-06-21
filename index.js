61fac2923003239149533bd056c628ea2bb58741
Mahen Seneviratne <mseneviratne@innovu.com>
2016-06-21 15:08:56 -0400

    reset console.log rule. change package version

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
 	'rules': {
 		// POSSIBLE ERRORS
 		// http://eslint.org/docs/rules/#possible-errors
-		'no-console': 0,
+		'no-console': 2,
 		'no-extra-parens': 0,
 		
 		// BEST PRACTICES
